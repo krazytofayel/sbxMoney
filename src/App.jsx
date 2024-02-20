88fe27e42b4752546a10d42956e205e2380a2063
krazytofayel <tofayel@krazyit.com>
2024-02-20 18:07:50 +0600

    update app.js for new components

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import Help from './Pages/Help/Help';
 import HelpDetail from './Pages/Help/HelpDetail/helpDetail';
 import AboutUs from './Pages/AboutUs/AboutUs';
 import Contact_Us from './Pages/Contact_Us/Contact_Us';
+import Condition_Of_Use from './Pages/Static_Page/Condition_Of_Use/Condition_Of_Use';
+import Privacy_Policy from './Pages/Static_Page/Privacy_Policy/Privacy_Policy';
+import Lagel_Agreement from './Pages/Static_Page/Lagel_Agreement/Lagel_Agreement';
+import User_Guide from './Pages/Static_Page/User_Guide/user_Guide';
+
 
 
 const App = () => {
@@ -17,7 +22,11 @@ const App = () => {
         <Route path="/help" element={<Help />} />
         <Route path="/helpdetails" element={<HelpDetail />} />
         <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="contactus" element={<Contact_Us/>}/>
+        <Route path="/contactus" element={<Contact_Us />} />
+        <Route path="/condition_of_use" element={<Condition_Of_Use />} />
+        <Route path="/privacy_policy" element={<Privacy_Policy />} />
+        <Route path="/lagel_agreement" element={<Lagel_Agreement />} />
+        <Route path="/user_guide" element={<User_Guide />} />
       </Routes>
 
     </>
