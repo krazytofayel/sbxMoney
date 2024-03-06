db8381326405d162a30bb6f3be11cf5082cb332f
krazytofayel <tofayel@krazyit.com>
2024-03-06 16:07:55 +0600

    contactus home page changes

--- src/Pages/Contact_Us/Contact_Us_Components/ConTact_Us_Home_Banner/Contact_Us_Home_Banner.jsx
+++ src/Pages/Contact_Us/Contact_Us_Components/ConTact_Us_Home_Banner/Contact_Us_Home_Banner.jsx
@@ -6,7 +6,7 @@ const Contact_Us_Home_Banner = () => {
     <>
       <section class="w-full flex bg-gradient-to-b from-[#cdffdb] to-transparent dark:bg-gray-900">
         <div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-          <div class="text-center space-y-5">
+          <div class="text-center space-y-5 h-auto">
             <h2 className='text-green-400 font-semibold'>Contact Us</h2>
             <p
               class="mt-1 text-4xl font-extrabold text-gray-900 dark:text-gray-100 sm:text-5xl sm:tracking-tight lg:text-6xl">
