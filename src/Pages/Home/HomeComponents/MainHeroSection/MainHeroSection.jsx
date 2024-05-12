280a738819bb8c845470fe0928e04ef474aacbbc
krazytofayel <tofayel@krazyit.com>
2024-05-12 17:33:55 +0600

    home section currency calculator change

--- src/Pages/Home/HomeComponents/MainHeroSection/MainHeroSection.jsx
+++ src/Pages/Home/HomeComponents/MainHeroSection/MainHeroSection.jsx
@@ -177,7 +177,7 @@ const MainHeroSection = () => {
                     </label>
                     <Select
                       id="countries"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full "
                       onChange={handleSelectFrom}
                       value={selectedCurrencyFrom}
                       options={cuntriesOptions}
@@ -192,7 +192,7 @@ const MainHeroSection = () => {
                     </label>
                     <Select
                       id="countries"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full "
                       onChange={handleSelectTo}
                       value={selectedCurrencyTo}
                       options={cuntriesOptions}
