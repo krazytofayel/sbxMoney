3f64f857311c2dfddae3a6a472ad7899c6feace9
krazytofayel <tofayel@krazyit.com>
2024-05-12 15:14:57 +0600

    dark mode design changes

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -1,37 +1,49 @@
-import React, { useState } from 'react'
-import Transfer_Ur_Money from './AboutUsComponents/Transfer_Ur_Money/Transfer_Ur_Money'
-import Vision from './AboutUsComponents/Vision/Vision'
-import Statistics from './AboutUsComponents/Statistics/Statistics'
-import Foundation from './AboutUsComponents/Foundation/Foundation'
-import Footer from '../../Components/ShareableComponents/Footer/Footer'
-import AboutUs_Home_Banner from './AboutUsComponents/AboutUs_Home_Banner/AboutUs_Home_Banner'
-import NavBar from '../../Components/ShareableComponents/NavBar/NavBar'
+import React, { useState, useEffect } from "react";
+import Transfer_Ur_Money from "./AboutUsComponents/Transfer_Ur_Money/Transfer_Ur_Money";
+import Vision from "./AboutUsComponents/Vision/Vision";
+import Statistics from "./AboutUsComponents/Statistics/Statistics";
+import Foundation from "./AboutUsComponents/Foundation/Foundation";
+import Footer from "../../Components/ShareableComponents/Footer/Footer";
+import AboutUs_Home_Banner from "./AboutUsComponents/AboutUs_Home_Banner/AboutUs_Home_Banner";
+import NavBar from "../../Components/ShareableComponents/NavBar/NavBar";
 
 const AboutUs = () => {
-    const [navfix, setNavfix] = useState(false);
+  const [navfix, setNavfix] = useState(false);
 
-  function setFixed() {
-    if (window.scrollY >= 70) {
-      setNavfix(true);
-      //console.log(scrollY)
-    } else {
-      setNavfix(false);
+  useEffect(() => {
+    function handleScroll() {
+      if (window.scrollY >= 70) {
+        setNavfix(true);
+      } else {
+        setNavfix(false);
+      }
     }
-  }
-  window.addEventListener("scroll", setFixed);
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener on unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-     <div className={`z-20 fixed top-0 bg-white w-full transition-all duration-300 ease-in-out ${navfix ? "shadow-lg bg-white" : ""}`}>
+      <div
+        className={`z-20 fixed top-0 bg-white w-full transition-all duration-300 ease-in-out ${navfix ? "shadow-lg bg-white" : ""}`}
+      >
         <NavBar />
       </div>
-      <AboutUs_Home_Banner />
-      <Transfer_Ur_Money />
-      <Vision />
-      <Statistics />
-      <Foundation />
-      <Footer />
+      <div >
+        <AboutUs_Home_Banner />
+        <Transfer_Ur_Money />
+        <Vision />
+        <Statistics />
+        <Foundation />
+        <Footer />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AboutUs
+export default AboutUs;
